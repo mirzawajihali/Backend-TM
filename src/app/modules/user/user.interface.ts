4fdba275340c969d71a5527928803a6a7cd6ade9
mirzawajihali <mirzawajihali00@gmail.com>
2025-07-12 23:20:49 +0600

    Feat user interface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,48 @@
+import { Types } from "mongoose";
+
+export enum Role {
+    SUPER_ADMIN = "SUPER_ADMIN",
+    ADMIN = "ADMIN",
+    USER = "USER",
+    GUIDE = "GUIDE"
+}
+
+export enum isActive {
+    ACTIVE = "ACTIVE",
+    INACTIVE = "INACTIVE",
+    BLOCKED = "BLOCKED"
+}
+
+
+
+
+//auth providers
+// email, Pass
+// google auth
+
+
+
+
+
+export interface IAuthProvider {
+    provider : string;
+    providerId : string;
+}
+
+
+export interface IUser {
+    name : string;
+    email : string;
+    password ?: string;
+    phone ?: string;
+    picture ?: string;
+    address ?: string;
+    inDeleted ?: boolean;
+    isActive ?: isActive;
+    isVarified ?: boolean;
+    auths : IAuthProvider[];
+    role : Role;
+    bookings ?: Types.ObjectId[];
+    guides ?: Types.ObjectId[];
+
+}

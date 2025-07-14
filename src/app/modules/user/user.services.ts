d48425681f288fb5f357d25cb1d85f02b15e7fbc
mirzawajihali <mirzawajihali00@gmail.com>
2025-07-15 01:22:11 +0600

    feat : encrypt password

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -2,6 +2,7 @@ import AppError from "../../errorHelpers/AppError";
 import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
 import httpStatus from "http-status-codes";
+import bcryptjs from "bcryptjs";
 
 
 
@@ -9,17 +10,22 @@ import httpStatus from "http-status-codes";
 
 const createUser = async (payload : Partial<IUser>) =>{
 
-    const { email , ...rest } = payload;
+    const { email , password, ...rest } = payload;
 
     const isUserExist = await User.findOne({email});
+
     if(isUserExist){
         throw new AppError(httpStatus.BAD_REQUEST, "User already exist");
     }
 
+    const hashedPassword = await bcryptjs.hash(password as string, 10);
+
+   
     const authProvider : IAuthProvider = {provider : "credentials", providerId : email as string}
 
     const user = await User.create({
             email, 
+            password : hashedPassword,
             auths : [authProvider],
             ...rest })
 

67283569693fcdba4efbe3525d7c9526b70ec2b8
mirzawajihali <mirzawajihali00@gmail.com>
2025-07-22 16:51:14 +0600

    feat : Structured error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,29 +1,46 @@
 import { NextFunction, Request, Response } from "express"
 import { env } from "../config/env"
 import AppError from "../errorHelpers/AppError";
+import { handleCastError } from "../helpers/handleCastError";
+import { handleDuplicateError } from "../helpers/handleDuplicateError";
+import { handleValidationError } from "../helpers/handleValidationError";
+import { handleZodError } from "../helpers/handleZodError";
+import { TErrorSources } from "../interfaces/error.types";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 export const globalErrorHandler = (err : any ,req : Request, res : Response, next : NextFunction ) =>{
 
     let statusCode =  500;
     let message = "Something went wrong";
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let errorSources: TErrorSources[] | undefined;
+
 
     if(err.code ===11000){
+        const simplifiedError = handleDuplicateError(err);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
 
-       const  duplicate = err.message.match(/"([^"]*)"/);
-        statusCode = 400;
-        message = `${duplicate[1] } already exists`;
     }
     else if(err.name ==="CastError"){
-
-        statusCode = 400;
-        message = `Invalid Mongodb ObjectId : ${err.value}`;
+        const simplifiedError = handleCastError(err);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
     }
     else if(err.name === "ValidationError"){
-        statusCode = 400;
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        message = Object.values(err.errors).map((val: any) => val.message).join(", ");
+        const simplifiedError = handleValidationError(err);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorSources = simplifiedError.errorSources as TErrorSources[];
+
     }
+    else if(err.name === "ZodError"){
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        errorSources = simplifiedError.errorSources as TErrorSources[]}
+
     else if(err instanceof AppError){
 
         statusCode = err.statusCode;
@@ -37,7 +54,8 @@ export const globalErrorHandler = (err : any ,req : Request, res : Response, nex
         res.status(statusCode).json({
             success : false,
             message,
-            err,
+            errorSources,
+            err : env.NODE_ENV === 'development' ? err : null,
             stack : env.NODE_ENV === 'development' ? err.stack : null
 
         })

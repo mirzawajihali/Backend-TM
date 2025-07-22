ea31898114083f34431c5bd8587379677df0ef0d
mirzawajihali <mirzawajihali00@gmail.com>
2025-07-22 16:23:28 +0600

    feat : mongoose error handling

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -7,10 +7,32 @@ export const globalErrorHandler = (err : any ,req : Request, res : Response, nex
 
     let statusCode =  500;
     let message = "Something went wrong";
-    if(err instanceof AppError){
+
+    if(err.code ===11000){
+
+       const  duplicate = err.message.match(/"([^"]*)"/);
+        statusCode = 400;
+        message = `${duplicate[1] } already exists`;
+    }
+    else if(err.name ==="CastError"){
+
+        statusCode = 400;
+        message = `Invalid Mongodb ObjectId : ${err.value}`;
+    }
+    else if(err.name === "ValidationError"){
+        statusCode = 400;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        message = Object.values(err.errors).map((val: any) => val.message).join(", ");
+    }
+    else if(err instanceof AppError){
 
         statusCode = err.statusCode;
         message = err.message;
+    }
+    else if(err instanceof Error){
+        statusCode = 500;
+        message = err.message
+
     }
         res.status(statusCode).json({
             success : false,

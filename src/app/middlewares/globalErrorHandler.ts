5599598b4254badd195db0ac12e545332ac9883f
mirzawajihali <mirzawajihali00@gmail.com>
2025-07-23 21:25:05 +0600

    feat : tour and division model and interface initialize

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express"
+import  { NextFunction, Request, Response } from "express"
 import { env } from "../config/env"
 import AppError from "../errorHelpers/AppError";
 import { handleCastError } from "../helpers/handleCastError";
@@ -9,6 +9,10 @@ import { TErrorSources } from "../interfaces/error.types";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 export const globalErrorHandler = (err : any ,req : Request, res : Response, next : NextFunction ) =>{
+
+    if(env.NODE_ENV === 'production') {
+        console.log(err)
+    }
 
     let statusCode =  500;
     let message = "Something went wrong";
---
+++ src/app/modules/tour/tour.interface.ts
@@ -0,0 +1,32 @@
+import { Types } from "mongoose";
+
+
+export interface ITourType {
+    name : string;
+
+}
+    
+export interface ITour{
+    title :string;
+    slug: string;
+    thumbnail : string;
+    description ?: string;
+    images ?: string[];
+    location? : string;
+    costFrom ?: number;
+    startDate ?: Date;
+    endDate ?: Date;
+    included ?: string[];
+    excluded ?: string[];
+    amenities ?: string[];
+    tourPlan ?: string[];
+    minAge ?: number;
+    division : Types.ObjectId;
+    duration ?: number;
+    maxGuest ?: number;
+    tourType : Types.ObjectId;
+
+    
+   
+
+}
